543c2df8fa47726211f86c975757383176823162
Shkelqim Memolla <shkelqim.memolla@gmail.com>
2014-07-18 16:25:37 +0300

    Added JavaScript

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,19 @@
   (function() {
-		addEventListener('polymer-ready', function() {
-		
-		var items = [];
+	addEventListener('polymer-ready', function() {
+	
+	var items = [];
 
-		$.getJSON("js/metadata.json", function(json) {
-			for(var i=0, j = 1; i<json.cams.length; i++, j++)
-				items.push([json.cams[i].url, "images/shelflist-small/raf" + j + ".png"]);
-		});
+	$.getJSON("js/metadata.json", function(json) {
+		for(var i=0, j = 1; i<json.cams.length; i++, j++)
+			items.push([json.cams[i].url, "images/shelflist-small/raf" + j + ".png"]);
+	});
 
-		
-	  	CoreStyle.g.items = items;
-		  addEventListener('template-bound', function(e) {
-		    e.target.g = CoreStyle.g;
-		    e.target.items = items;
-		  });
-		});
+	
+  	CoreStyle.g.items = items;
+	  addEventListener('template-bound', function(e) {
+	    e.target.g = CoreStyle.g;
+	    e.target.items = items;
+	  });
+	});
 
-	  })();
+  })();
